9971c9a2adcedde3cb46c325683e16951be235cb
Yinfeng Guo <guoyinfeng@live.com>
2014-05-05 17:27:10 +0800

    JSON.stringify

---
+++ json/stringify.js
@@ -0,0 +1,12 @@
+/**
+ * Created by YinFeng-Guo on 14-5-5.
+ */
+var user = {
+    "firstname":"Yinfneg",
+    "lastname":"Guo",
+    "age":20
+}
+
+var userYinfeng = JSON.stringify(user);
+
+console.log('user::'+userYinfeng);
